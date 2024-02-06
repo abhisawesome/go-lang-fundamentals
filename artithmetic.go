ff3c4445de1be21fa6791d9321221161acb8fe49
Abhijith v <abhijithababhijith@gmail.com>
2024-02-06 21:59:03 +0530

    arithmetic

---
+++ artithmetic.go
@@ -0,0 +1,24 @@
+// Perform basic arithmetic operations (+, -, *, /) on these variables and print the results.
+package main
+
+import "fmt"
+
+
+func main(){
+
+	i :=1
+	j :=2
+
+	fmt.Println("i + j = ",i+j)
+	fmt.Println("i - j = ",i-j)
+	fmt.Println("i * j = ",i*j)
+	fmt.Println("i / j = ",i/j)
+	fmt.Println("i % j = ",i%j)
+	
+
+	k:="hello"
+	l:="world"
+
+	fmt.Println("k + l = ",k+l)
+	
+}
